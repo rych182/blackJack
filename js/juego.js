8488ba896eda46a2dbf619ab7cfa842f4cf7e6bc
El Richo <ricgarridoc@hotmail.com>
2020-05-03 21:37:48 -0500

    I added more functions

--- js/juego.js
+++ js/juego.js
@@ -26,22 +26,47 @@ crearDeck();
 //Esta función me permite tomar una carta del deck y descontarla del deck
 const pedirCarta = () => {
 
-    if (deck.length === 0) {
-        throw "No hay cartas en el deck"; // throw muestra un error en consola
+        if (deck.length === 0) {
+            throw "No hay cartas en el deck"; // throw muestra un error en consola
+        }
+
+        const carta = deck.pop();
+
+        console.log(deck);
+        console.log(carta);
+        return carta;
     }
-
-    const carta = deck.pop();
-
-    console.log(deck);
-    console.log(carta);
-    return carta;
-}
+    /*
+    Verifica que vaya descontando una carta hasta que te quedes sin cartas
+    for (let i = 0; i < 60; i++) {
+        pedirCarta();
+    }
+    */
+pedirCarta();
 
 /*
-Verifica que vaya descontando una carta hasta que te quedes sin cartas
-
-for (let i = 0; i < 60; i++) {
-    pedirCarta();
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    let puntos = 0;
+    //probamos si la carta tiene un número o una letra J,Q,K,A
+    if (isNaN(valor)) {
+        puntos = (valor === 'A') ? 11 : 10;
+    } else {
+        console.log("Es un número");
+        //Se multiplica por 1, porque si no, nos regresa un string
+        puntos = valor * 1;
+    }
+    console.log(puntos);
 }
+valorCarta('9D');
 */
-pedirCarta();
+//Codigo más elegante de la función "Valor carta", que usa el valor de la carta usando la función pedirCarta()
+
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor)) ?
+        (valor === 'A') ? 11 : 10 :
+        valor * 1;
+}
+const valor = valorCarta(pedirCarta());
+console.log({ valor });
