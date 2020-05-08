129b321cfd5f967b744c1214d0acf88155db5958
ricgarrido <ricgarridoc@hotmail.com>
2020-05-08 17:56:14 -0500

    Refatorización terminada

--- js/juego.js
+++ js/juego.js
@@ -4,10 +4,8 @@ D = Diamonds
 H = Hearts
 S = Spades
 */
-
-(() => {
+const miModulo = (() => {
     'use strict'
-
     let deck = [];
     const tipos = ['C', 'D', 'H', 'S']; //corazones, diamantes, espadas, treboles
     const especiales = ['A', 'J', 'Q', 'K'];
@@ -16,11 +14,10 @@ S = Spades
     //let puntosComputadora = 0;
     let puntosJugadores = [];
 
-
     //Referencias del html
-    const btnNuevo = document.querySelector('#btnNuevo');
     const btnPedir = document.querySelector('#btnPedir');
     const btnDetener = document.querySelector('#btnDetener');
+    const btnNuevo = document.querySelector('#btnNuevo');
     const divCartasJugadores = document.querySelectorAll('.divCartas');
     const puntosHtml = document.querySelectorAll('small');
 
@@ -31,38 +28,36 @@ S = Spades
         for (let i = 0; i < numJugadores; i++) {
             puntosJugadores.push(0);
         }
-
         //puntosHtml.forEach(elem => elem.innerHTML = 0); Es lo mismo que
         //puntosHtml[0].innerText = 0;
         //puntosHtml[1].innerText = 0;
         //forEach nos permite ejecutar un callback por cda elemento que se encuentre en cada puntosHtml
-        puntosHtml.forEach(elem => elem.innerHTML = 0);
+        puntosHtml.forEach(elem => elem.innerText = 0);
         divCartasJugadores.forEach(elem => elem.innerHTML = '');
-
         btnPedir.disabled = false;
         btnDetener.disabled = false;
     }
 
     //Función que crea un nuevo Deck
     const crearDeck = () => {
-        deck = [];
-        //Las cartas empiezan en 2
-        for (let i = 2; i <= 10; i++) {
-            //este for es para que le indiquemos los 4 tipos de carta en cada número
+            deck = [];
+            //Las cartas empiezan en 2
+            for (let i = 2; i <= 10; i++) {
+                //este for es para que le indiquemos los 4 tipos de carta en cada número
+                for (let tipo of tipos) {
+                    deck.push(i + tipo);
+                }
+            }
+            //Este for agrega al deck los aces, J,Q y K con sus diferentes tipos
             for (let tipo of tipos) {
-                deck.push(i + tipo);
+                for (let especiale of especiales) {
+                    deck.push(especiale + tipo);
+                }
             }
+            //shuffle es una función de la libreria underscore.js que sirve para barajear las cartas, porque si no, siempre apareceran en orden
+            return _.shuffle(deck); // retornas deck para que en la siguiente función puedas descontar una carta del deck
         }
-        //Este for agrega al deck los aces, J,Q y K con sus diferentes tipos
-        for (let tipo of tipos) {
-            for (let especiale of especiales) {
-                deck.push(especiale + tipo);
-            }
-        }
-        //shuffle es una función de la libreria underscore.js que sirve para barajear las cartas, porque si no, siempre apareceran en orden
-        return _.shuffle(deck); // retornas deck para que en la siguiente función puedas descontar una carta del deck
-    }
-    crearDeck();
+        //crearDeck();
 
     //Esta función me permite tomar una carta del deck y descontarla del deck
     const pedirCarta = () => {
@@ -81,7 +76,7 @@ S = Spades
             pedirCarta();
         }
         */
-    pedirCarta();
+        //pedirCarta();
 
     /*
     const valorCarta = (carta) => {
@@ -133,10 +128,7 @@ S = Spades
     }
 
     const determinarGanador = () => {
-
         const [puntosMinimos, puntosComputadora] = puntosJugadores;
-
-
         setTimeout(() => { //Lo uso para que aparezcan primero las cartas de la computadora antes que el alert
             if (puntosComputadora === puntosMinimos) {
                 alert('Nadie gana =/');
@@ -152,24 +144,20 @@ S = Spades
 
     //Turno de la computadora, cuando el jugador pierde, alcance los 21 ó presione el boton detener
     const turnoComputadora = (puntosMinimos) => {
-
         let puntosComputadora = 0;
         do {
-            const puntosComputadora = 0;
             const carta = pedirCarta();
+            //const puntosComputadora = 0;
             //Se le pone puntosJugadores.length porque no sabemos el número de jugadores que haya dentro del arreglo puntosJugadores[]
             //Se le pone el -1 porque al usar .length empezará a contar las posiciones desde el 1, pero los arreglos comienzan en 0, entonces se le resta 1
             puntosComputadora = acumularPuntos(carta, puntosJugadores.length - 1);
             crearCarta(carta, puntosJugadores.length - 1);
-
+            /*
             if (puntosMinimos > 21) {
                 break;
-            }
-
+            }*/
         } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
-
         determinarGanador();
-
     }
 
     //Eventos
@@ -199,12 +187,15 @@ S = Spades
         btnPedir.disabled = true;
         btnDetener.disabled = true;
         //llamo al turno de la computadora, donde
-        turnoComputadora(puntosJugador);
+        turnoComputadora(puntosJugadores[0]);
     });
 
-    btnNuevo.addEventListener('click', () => {
-
-        inicializarJuego();
-
-    })
+    /*
+        btnNuevo.addEventListener('click', () => {
+            inicializarJuego();
+        });
+    */
+    return {
+        nuevoJuego: inicializarJuego
+    };
 })();
